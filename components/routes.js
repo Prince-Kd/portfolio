4cd0e50b36cc948624b0a888b3654e9f2c27344c
Mawuli Kwadzofio <princekd131@gmail.com>
2022-09-06 10:41:49 +0000

    Update routes.js

--- components/routes.js
+++ components/routes.js
@@ -7,14 +7,12 @@ export default function Routes() {
   return (
     <div className="flex flex-col items-stretch justify-start">
       <Link href={"#about"}>
-        <a
-          className={`py-2 px-1 my-1 text-left text-gray-500 cursor-pointer active:bg-gray-100 focus:outline-none focus:bg-black focus:pl-2 focus:text-white `}
-        >
+        <a className={`py-2 px-1 my-1 text-left text-gray-500 cursor-pointer active:bg-gray-100 focus:outline-none focus:bg-gray-800 focus:pl-2 focus:text-white `}>
           About
         </a>
       </Link>
       <Link href={"#work"}>
-        <a className={`py-2 px-1 my-1 text-left text-gray-500 cursor-pointer active:bg-gray-100 focus:outline-none focus:bg-black focus:pl-2 focus:text-white `}>Recent work</a>
+        <a className={`py-2 px-1 my-1 text-left text-gray-500 cursor-pointer active:bg-gray-100 focus:outline-none focus:bg-gray-800 focus:pl-2 focus:text-white `}>Recent work</a>
       </Link>
       {/* <Link href={"#about"}><li>About</li></Link> */}
     </div>
